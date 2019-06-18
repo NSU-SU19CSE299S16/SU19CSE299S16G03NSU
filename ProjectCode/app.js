1be2a01f8b3ae24ce15a6d8fbd5404530d963b33
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-06-19 02:21:19 +0600

    test navbar and footer added

--- ProjectCode/app.js
+++ ProjectCode/app.js
@@ -14,6 +14,10 @@ app.get("/", function(req, res){
 app.get("/signUp", function(req, res){
   res.render("signUp");
 });
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
