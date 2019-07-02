f5e1258c9ad9c6ff13bb81700586c5eba21e1392
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-07-02 23:11:40 +0600

    added signup #7

--- PROJECT CODE/Populace/api/views.py
+++ PROJECT CODE/Populace/api/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'api/index.html')
+
+def signup(request):
+    return render(request,'api/signup.html')
