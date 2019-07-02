41bbe48a58c0bac57e3faa880652efbc0012f5d4
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-07-03 01:13:17 +0600

    Piazza-api integration fixed #9

--- PROJECT CODE/Populace/api/urls.py
+++ PROJECT CODE/Populace/api/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('signup/',views.signup, name='signup')
+    path('signup/',views.signup, name='signup'),
+    path('profile/',views.profile, name='profile'),
     ]
--- PROJECT CODE/Populace/api/views.py
+++ PROJECT CODE/Populace/api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from piazza_api import Piazza
 # Create your views here.
 def home(request):
     return render(request,'api/index.html')
 
 def signup(request):
     return render(request,'api/signup.html')
+
+def profile(request):
+    p = Piazza()
+    p.user_login()
+    return render(request,'api/profile.html')
