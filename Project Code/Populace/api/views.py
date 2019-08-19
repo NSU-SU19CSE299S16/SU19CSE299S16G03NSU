a433b72956e1f08fc133236009d0dbdc8c325a8b
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-08-20 04:18:24 +0600

    Feature #27 : added authorization via method v2

--- Project Code/Populace/api/views.py
+++ Project Code/Populace/api/views.py
@@ -185,6 +185,28 @@ def piazza_posts(request,pk = None):
 # Function for google classroom api functionality starts here
 def profile_g(request):
     if request.method =='POST':
-        pass
+        # If modifying these scopes, delete the file token.pickle.
+        SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly']
+    """Shows basic usage of the Classroom API.
+    Prints the names of the first 10 courses the user has access to.
+    """
+        creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
     else:
         return render(request,'api/profile.html')        # form_g = googleLoginForm()
