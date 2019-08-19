85a063a8773614aaccbe408c0d94ec5d98b0e44d
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-08-20 03:42:09 +0600

    Feature #27 : Added authorization via quickstart.py in test function

--- Project Code/Populace/api/views.py
+++ Project Code/Populace/api/views.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from django.shortcuts import render,redirect
 from piazza_api import Piazza
 from .forms import piazzaLoginForm,googleLoginForm
@@ -13,6 +14,15 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 from django.urls import resolve
 import os
+# test
+
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
@@ -173,12 +183,55 @@ def oauth2callback(request):
         print('somethign ')
         print("blaad,lasd," + results)
         courses = results.get('courses', [])
-
+        test = []
         if not courses:
             print('No courses found.')
         else:
             print('Courses:')
             for course in courses:
-                print(course['name'])
+                test.append(course['name'])
 
-    return render(request,'api/google-class.html')
+    return render(request,'api/google-class.html',{'test':test})
+
+
+
+
+def test(request):
+
+
+    """Shows basic usage of the Classroom API.
+    Prints the names of the first 10 courses the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('classroom', 'v1', credentials=creds)
+
+    # Call the Classroom API
+    results = service.courses().list(pageSize=10).execute()
+    courses = results.get('courses', [])
+    g_course =[]
+    if not courses:
+        print('No courses found.')
+    else:
+        print('Courses:')
+        for course in courses:
+            g_course.append(course['name'])
+
+    return render(request,'api/google-class.html',{'class_name':g_course})
